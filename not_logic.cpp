dad166f9b4926c29a23ea7043de54f945ada17e7
Denay2468 <denayaung13@gmail.com>
2026-04-07 10:00:09 +0630

    make clear with space and syntax

--- not_logic.cpp
+++ not_logic.cpp
@@ -7,17 +7,21 @@ int main(){
 
     cout<<"Enter Indonesian Language Score : ";
     cin>>ScoreB;
+    
     cout<<"Enter English Score : ";
     cin>>ScoreA;
 
     Avarage = (ScoreA + ScoreB) / 2;
 
-    if (!(Avarage <60))
-    {
+    if (!(Avarage <60)){
+
         status = "Passed";
+
     } else {
+
         status = "Failed";
     }
+
     cout<<"Status: "<<status<<"with an average score of "<<Avarage<<endl;
 
 
