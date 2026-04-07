5b9f0cd4c75c3927dc8cea109a5f3faf1c307036
Denay2468 <denayaung13@gmail.com>
2026-04-07 09:36:44 +0630

    add for loop

--- loop.cpp
+++ loop.cpp
@@ -5,4 +5,11 @@ using namespace std;
 int main(){
     int x;
     cout<<"Loop for "<<endl;
+    for (int i=0; i<4; i++){
+        cout<<"Infromation Technology UMY"<<endl;
+
+    
 }
+cout<<endl;
+
+srand(time(0));
